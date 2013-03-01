b9736a2cf996f817cb63a4c1b2a3ab3bdc62a54c
Thomas Levine <occurrence@thomaslevine.com>
2013-03-01 17:27:42 -0500

    oeuoeu

--- participation.r
+++ participation.r
@@ -1,26 +1,4 @@
 library(plyr)
-# [27,] "A4c_PBCbarriers"    
-# 
-# [28,] "A4c_1.financial"    
-# [29,] "A4c_2.jobsecurity"  
-# [30,] "A4c_3.childcare"    
-# [31,] "A4c_4.time"         
-# [32,] "A4c_5.support"      
-# [33,] "A4c_6.work"         
-# [34,] "A4c_7.info"         
-# [35,] "A4c_8.school"       
-# [36,] "A4c_9.surveillance" 
-# [37,] "A4c_10.police"      
-# [38,] "A4c_11.arrest"      
-# [39,] "A4c_12.other"       
-# [40,] "A4c_12.explain"    
-# 
-# [59,] "A8.ATTEND_YN"       
-# [60,] "A8.negative.perception"          
-# [61,] "A8.attend"          
-# [62,] "A8.explain"   
-
-
 all <- read.csv('occuPARcleandata.csv')
 
 # Only not participate
@@ -33,9 +11,9 @@ o <- data.frame(
   has.negative.perception  = rowSums(not.participate[c('A7c_1.ineffective', 'A7c_2.noconcern', 'A7c_3.unfocused', 'A7c_4.disruptive', 'A7c_5.other')]) > 0
 )
 
-sample.probabilities <- ddply(o, c('has.barrier', 'no.never'),
+sample.probabilities <- ddply(o, c('has.barrier', 'has.negative.perception'),
                    function(df) {
-                       c(p.no.never = paste(100 * round(mean(df$has.negative.perception), 3), '%', sep = ''))
+                       c(p.no.never = paste(100 * round(mean(df$no.never), 3), '%', sep = ''))
                    })
 
 # Logistic regression
@@ -43,13 +21,14 @@ logit.alternative <- glm(no.never ~ has.barrier * has.negative.perception,
                          family = 'binomial', data = o)
 logit.null        <- glm(no.never ~ has.negative.perception,
                          family = 'binomial', data = o)
-#print(summary(logit.alternative))
-#print(anova(logit.alternative, logit.null))
 
 # Ordinary least square regression
 ols.alternative <- lm(no.never ~ has.barrier * has.negative.perception, data = o)
 ols.null        <- lm(no.never ~ has.negative.perception, data = o)
 
+print(sample.probabilities)
+#print(summary(logit.alternative))
+#print(anova(logit.alternative, logit.null))
 print(summary(ols.alternative))
 print(anova(ols.alternative, ols.null))
 

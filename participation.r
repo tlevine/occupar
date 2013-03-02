9b232e3fbf189b0ca25bee6b8e657a76ae2eb947
Thomas Levine <occurrence@thomaslevine.com>
2013-03-02 14:38:39 -0500

    yay

--- participation.r
+++ participation.r
@@ -6,18 +6,25 @@ all <- read.csv('occuPARcleandata.csv')
 not.participate <- subset(all, A8.ATTEND_YN == 0)
 
 # New variables
+q.7c <- c('A7c_1.ineffective', 'A7c_2.noconcern', 'A7c_3.unfocused', 'A7c_4.disruptive', 'A7c_5.other')
 o <- data.frame(
-  has.life.stress          = not.participate$A4c_PBClife.stresss > 0,
+  has.life.stress          = not.participate$A4c_PBCbarriers > 0,
   no.never                 = not.participate$A8.attend == 3,
-  has.negative.perception  = rowSums(not.participate[c('A7c_1.ineffective', 'A7c_2.noconcern', 'A7c_3.unfocused', 'A7c_4.disruptive', 'A7c_5.other')]) > 0
+  has.negative.perception  = rowSums(not.participate[q.7c]) > 0
 )
 
 # A summary
-sample.probabilities <- ddply(
+sample.proportions <- ddply(
   o,
   c('has.life.stress', 'has.negative.perception'),
-  function(df) { c(p.no.never = paste(100 * round(mean(df$no.never), 3), '%', sep = '')) }
+  function(df) { c(p.no.never = mean(df$no.never)) }
 )
+sample.proportions.pretty <- sample.proportions
+sample.proportions.pretty$p.no.never <-
+    paste(100 * round(sample.proportions.pretty$p.no.never, 3), '%', sep = '')
+    
+sample.proportions.plot <- ggplot(sample.proportions) +
+    aes(x = has.negative.perception, group = has.life.stress, y = p.no.never, lty = has.life.stress) + geom_line()
 
 # Logistic regression
 logit.alt  <- glm(no.never ~ has.life.stress * has.negative.perception,

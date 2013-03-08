de78199363d064319712012a59c1f13c2d8c0819
Thomas Levine <occurrence@thomaslevine.com>
2013-03-08 08:40:18 -0500

    capitalize

--- participation.r
+++ participation.r
@@ -10,7 +10,7 @@ not.participate <- subset(all, A8.ATTEND_YN == 0)
 # New variables
 q.7c <- c('A7c_1.ineffective', 'A7c_2.noconcern', 'A7c_3.unfocused', 'A7c_4.disruptive', 'A7c_5.other')
 o <- data.frame(
-  has.external.stress          = as.numeric(not.participate$A4c_PBCbarriers > 0),
+  has.external.stress      = as.numeric(not.participate$A4c_PBCbarriers > 0),
   no.never                 = as.numeric(not.participate$A8.attend == 3),
   has.negative.perception  = as.numeric(rowSums(not.participate[q.7c]) > 0)
 )
@@ -31,7 +31,7 @@ sample.proportions.plot <- ggplot(sample.proportions) +
     scale_y_continuous('Proportion selecting "never" rather than a simple "no".', labels = percent, limits = 0:1) +
     theme_bw() +
     labs(x = 'Has negative perception\n(checked at least one box)',
-         lty = 'Has external.stress\n(checked at least\none box)',
+         lty = 'Has external stress\n(checked at least\none box)',
          title = 'Negative perception of OccupyCUNY is\nmore strongly associated with participation\nwhen external.stress is greater.')
 
 # Logistic regression
